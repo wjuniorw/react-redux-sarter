ff0395ce845f7be6b9008c81a3bdbb3c214b0309
wjuniorw <wilson.junior90@gmail.com>
2021-03-20 23:27:22 -0300

    1 file modified

--- src/services/auth.js
+++ src/services/auth.js
@@ -2,22 +2,19 @@ import Api from './axios'
 
 const login = user => {
   console.log('<===== login service ====>', user)
-  localStorage.setItem('App@User', JSON.stringify(user))
-  localStorage.setItem(
-    'App@token',
-    'eyJhbGciOiJub25lIn0.eyJ1cm46ZXhhbXBsZTpjbGFpbSI6dHJ1ZSwiaWF0IjoxNjE1NzcwOTc5LCJpc3MiOiJ1cm46ZXhhbXBsZTppc3N1ZXIiLCJhdWQiOiJ1cm46ZXhhbXBsZTphdWRpZW5jZSIsImV4cCI6MTYxNTc3ODE3OX0'
-  )
   const token =
     'eyJhbGciOiJub25lIn0.eyJ1cm46ZXhhbXBsZTpjbGFpbSI6dHJ1ZSwiaWF0IjoxNjE1NzcwOTc5LCJpc3MiOiJ1cm46ZXhhbXBsZTppc3N1ZXIiLCJhdWQiOiJ1cm46ZXhhbXBsZTphdWRpZW5jZSIsImV4cCI6MTYxNTc3ODE3OX0'
   const refreshToken =
     'eyJhbGciOiJub25lIn0.eyJ1cm46ZXhhbXBsZTpjbGFpbSI6dHJ1ZSwiaWF0IjoxNjE1NzcwOTc5LCJpc3MiOiJ1cm46ZXhhbXBsZTppc3N1ZXIiLCJhdWQiOiJ1cm46ZXhhbXBsZTphdWRpZW5jZSIsImV4cCI6MTYxNTc3ODE3OX0'
+  localStorage.setItem('App@User', JSON.stringify(user))
+  localStorage.setItem('App@token', token)
   return { data: { user, token, refreshToken } }
 }
 
 const AuthModule = {
   // login: user => Api.post("/login", user),
   login,
-  signup: () => null,
+  signup: user => Api.post('/signup', user),
   getToken: () => null,
   refreshToken: () => null,
 }
